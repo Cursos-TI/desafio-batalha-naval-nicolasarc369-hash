09292407c90197052499d1de5cfba38b4db61f97
nicolasarc369-hash <nicolas.arc369@gmail.com>
2026-06-20 05:44:19 +0000

    nivel aventureiro finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS 10
+#define letra 10
+
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
@@ -13,29 +17,76 @@ int main() {
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
     // inicio da posição x
-    char letras [5] = {'A', 'B', 'C', 'D', 'E'};
+//     char letras [5] = {'A', 'B', 'C', 'D', 'E'};
 
-    printf("\t");
-    for (int i = 0;i < 5;i++){
-        printf("%c\t", letras[i]);
+//     printf("\t");
+//     for (int i = 0;i < 5;i++){
+//         printf("%c\t", letras[i]);
+//     }
+
+//     printf("\n");
+//     // fim da posição x
+
+//     //inicio posição y
+//     int matriz[5][5] = {
+//         {0, 0, 3, 0, 0},
+//         {0, 0, 3, 0, 0},
+//         {0, 0, 3, 0, 0},
+//         {0, 0, 0, 0, 0},
+//         {0, 3, 3, 3, 0}
+//     };
+
+//     for(int i = 0;i < 5;i++){
+//         printf("%d\t", i + 1);
+
+//         for(int j = 0; j < 5; j++){
+//             printf("%d\t", matriz[i][j]);
+//         }
+//         printf("\n");
+//     }
+//    printf("\n");
+//    //fim posição y
+   
+    
+//     return 0;
+
+    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
+    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
+    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+    char letras [letra];
+
+    for (int i = 0; i < letra; i++) {
+        letras[i] = 'A' + i;
     }
 
+    printf("\t");
+    for (int i = 0; i < letra; i++) {
+    printf("%c\t", letras[i]);
+    }
     printf("\n");
     // fim da posição x
 
     //inicio posição y
-    int matriz[5][5] = {
-        {0, 0, 3, 0, 0},
-        {0, 0, 3, 0, 0},
-        {0, 0, 3, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 3, 3, 3, 0}
+    
+    int matriz[LINHAS][COLUNAS] = {
+        {3, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+        {0, 3, 0, 0, 0, 0, 0, 0, 3, 0},
+        {0, 0, 3, 0, 0, 0, 0, 3, 0, 0},
+        {0, 0, 0, 3, 0, 0, 3, 0, 0, 0},
+        {0, 0, 0, 0, 3, 3, 0, 0, 0, 0},
+        {0, 0, 0, 0, 3, 3, 0, 0, 0, 0},
+        {0, 0, 0, 3, 0, 0, 3, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0, 0, 3, 0, 0},
+        {0, 3, 0, 0, 0, 0, 0, 0, 3, 0},
+        {3, 0, 0, 0, 0, 0, 0, 0, 0, 3},
     };
 
-    for(int i = 0;i < 5;i++){
+    for(int i = 0; i < LINHAS;i++){
         printf("%d\t", i + 1);
 
-        for(int j = 0; j < 5; j++){
+        for(int j = 0; j < COLUNAS; j++){
             printf("%d\t", matriz[i][j]);
         }
         printf("\n");
@@ -45,11 +96,6 @@ int main() {
    
     
     return 0;
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
